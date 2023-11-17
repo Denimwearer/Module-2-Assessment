0552879aae69d8c2541bd93081e9c6bebadad135
Denimwearer <CorderoDixon12@yahoo.com>
2023-11-17 13:20:57 -0500

    finished employees section

--- employees.js
+++ employees.js
@@ -20,9 +20,15 @@
 */
 
 //CODE HERE
-
-
-
+class Employee {
+  constructor(name, shifts) {
+    this.name = name;
+    this.shifts = shifts;
+  }
+  getSchedule = () => {
+    console.log(`${this.name} works on ${this.shifts}`);
+  };
+}
 /*
     Create a new instance of your class.
     Save it to a variable called `empOne`.
@@ -34,15 +40,14 @@
 */
 
 //CODE HERE
-
+const empOne = new Employee("Jess", "weekday mornings");
 /*
     Call the `getSchedule` method on the
     `empOne` object.
 */
 
 //CODE HERE
-
-
+empOne.getSchedule();
 /*
     Make a copy of the empOne object
     using the spread operator.
@@ -56,8 +61,9 @@
 */
 
 //CODE HERE
+const empTwo = { ...empOne, name: "Nick" };
 
-
+console.log(empTwo);
 
 //////////////////PROBLEM 2////////////////////
 /*  
@@ -83,9 +89,19 @@
 */
 
 //CODE HERE
+class Manager extends Employee {
+  constructor(name, shifts, employees) {
+    super(name, shifts);
+    this.employees = employees;
+  }
+  getEmployees = () => {
+    console.log(`${this.name} manages ${this.employees}`);
+  };
 
-
-
+  addEmployees = (emp) => {
+    this.employees.push(emp);
+  };
+}
 /*
     Create a new instance of your class.
     Save it to a variable called `manager`.
@@ -98,23 +114,25 @@
 */
 
 //CODE HERE
-
-
+const manager = new Manager("Winston", "weekday mornings", [
+  "Cece",
+  "Schmidit",
+]);
 /*
     Call the `getEmployees` method on the
     `manager` object.  
 */
 
 //CODE HERE
-
+manager.getEmployees();
 /*
     Call the `addEmployee` method on the 
     `manager` object passing in the string 
     'Coach' or whatever name you'd like.
 */
 
-//CODE HERE 
-
+//CODE HERE
+manager.addEmployees("Coach");
 /*
     Call the `getEmployees` method on the
     `manager` object again to confirm 
@@ -122,3 +140,5 @@
 */
 
 //CODE HERE
+
+manager.getEmployees();
